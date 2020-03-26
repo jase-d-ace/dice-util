0a9d22d1b7997119469df08f317df890186eb8d6
Jason Andrada <jpa323@nyu.edu>
2020-03-25 21:18:06 -0400

    rename file

---
+++ rollStats.js
@@ -0,0 +1,19 @@
+// roll a bunch of the same dice and return an array of four
+const rollStatLine = diceType => [Math.ceil(Math.random() * diceType), Math.ceil(Math.random() * diceType), Math.ceil(Math.random() * diceType), Math.ceil(Math.random() * diceType)];
+
+// roll a bunch of the same dice a bunch of times and return an array of the amount of times
+const rollTime = (times, cb, cbArg) => {
+  let holder = [];
+
+  for (let i = 0 ; i <= times ; i++) {
+    holder.push(cb(cbArg));
+  };
+
+  return holder;
+};
+
+
+module.exports = {
+  rollStatLine,
+  rollTime
+}
