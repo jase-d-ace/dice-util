ab56311b8cdf6b709e809362abe9f5c32d311975
Jason Andrada <jpa323@nyu.edu>
2020-03-25 21:19:12 -0400

    change variables to reflect new file

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const { rollDice } = require('./roll');
-const { rollMultipleDice, rollTime } = require('./rollMultiple');
+const { rollStatLine, rollTime } = require('./rollStats');
 const { D_20, D_4, D_6, D_8, D_10, D_12, STAT_GENERATION_LOOP, ADVANTAGE_DISADVANTAGE } = require('./constants');
 
 // roll character stats
-rollTime(STAT_GENERATION_LOOP, rollMultipleDice, D_6);
+rollTime(STAT_GENERATION_LOOP, rollStatLine, D_6);
 
 // roll at (dis)advantage
 rollTime(ADVANTAGE_DISADVANTAGE, rollMultipleDice, D_20);
