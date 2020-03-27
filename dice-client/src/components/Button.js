ed4901ae00f0f0f9101591fbff889ee3049c28ff
Jason Andrada <jpa323@nyu.edu>
2020-03-27 14:42:34 -0400

    lift state from button to app

--- dice-client/src/components/Button.js
+++ dice-client/src/components/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { rollTime } from '../services'
 
-const Button = props => <button onClick={() => rollTime(props.timeRoll, props.callback, props.button)}>{props.text}</button>
+const Button = ({ button, text, callback, setter, timeRoll }) => <button onClick={() => setter(rollTime(timeRoll, callback, button))}>{text}</button>
 
 export default Button;
