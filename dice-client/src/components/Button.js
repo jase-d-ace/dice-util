e3edbf2382283e5b08ab52a35ced58c0b3eeb9de
Jason Andrada <jpa323@nyu.edu>
2020-03-27 12:32:11 -0400

    configure button to use props for callbacks

--- dice-client/src/components/Button.js
+++ dice-client/src/components/Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import { rollTime } from '../services'
 
-const Button = props => <button onClick={props.onClick}>D{props.button}</button>
+const Button = props => <button onClick={() => rollTime(props.timeRoll, props.callback, props.button)}>{props.text}</button>
 
 export default Button;
