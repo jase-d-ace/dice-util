78887a09c7e7a4dd91517b9d3beea93b0bf4467a
Jason Andrada <jpa323@nyu.edu>
2020-04-12 15:37:28 -0400

    add propTypes object

--- dice-client/src/components/Button.js
+++ dice-client/src/components/Button.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import { rollTime } from '../services'
+import { rollTime } from '../services';
+import PropTypes from 'prop-types';
 
-const Button = ({ button, text, callback, setter, timeRoll, idVal }) => <button onClick={() => setter(rollTime(timeRoll, callback, button))} className="dice-button" id={idVal}>{text}</button>
+const Button = ({ button, text, callback, setter, timeRoll, idVal }) => <button onClick={() => setter(rollTime(timeRoll, callback, button))} className="dice-button" id={idVal}>{text}</button>;
+
+Button.propTypes = {
+  button: PropTypes.number,
+  text: PropTypes.string,
+  setter: PropTypes.func.isRequired,
+  idVal: PropTypes.string,
+  callback: PropTypes.func.isRequired,
+  timeRoll: PropTypes.number
+}
 
 export default Button;
