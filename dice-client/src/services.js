d506d353233fc16894f6bc9d2a6b46a656fae52d
Jason Andrada <jpa323@nyu.edu>
2020-04-12 21:43:41 -0400

    clean up helper a little more

--- dice-client/src/services.js
+++ dice-client/src/services.js
@@ -4,15 +4,6 @@ export const rollDice = diceType => Math.ceil(Math.random() * diceType);
 // roll a single die multiple times and return an array of length 4 with die rolls
 export const rollStatLine = diceType => [Math.ceil(Math.random() * diceType), Math.ceil(Math.random() * diceType), Math.ceil(Math.random() * diceType), Math.ceil(Math.random() * diceType)];
 
-// roll a die or multiple dice at the same time
-// arguments will generally be supplied from the constants file
-/* arguments: 
- * times: generally will either be 2 or 6, depending on reason to roll multiple times. 2 for advantage, 6 for character stat generation
- * cb: callback function dictating whether to roll a single die, or a series of d6
- * cbArg: the type of die rolled
-*/
-
-
 // helper function to check if a number is greater than 7
 // in context, this is to see if a player has rolled less than 7 on any given stat
 // it's meant to take out these rolls and allow a "reroll" for any stat that would end up being really bad
@@ -26,7 +17,7 @@ const reRoll = (newArr) => {
   const [least, ...highest] = newStatLine;
 
   return checkRolls(highest, newArr);
-}
+};
 
 
 // bias function that decides whether an array is acceptable (i.e. has a total greater than 7)
@@ -40,7 +31,15 @@ const checkRolls = (arr, holder) => {
     return true;
   }
   return false;
-}
+};
+
+// roll a die or multiple dice at the same time
+// arguments will generally be supplied from the constants file
+/* arguments: 
+ * times: generally will either be 2 or 6, depending on reason to roll multiple times. 2 for advantage, 6 for character stat generation
+ * cb: callback function dictating whether to roll a single die, or a series of d6
+ * cbArg: the type of die rolled
+*/
 
 export const rollTime = (times, cb, cbArg) => {
   // empty array to hold values
@@ -61,10 +60,10 @@ export const rollTime = (times, cb, cbArg) => {
         found = reRoll(holder);
       }
       // end result is an array of arrays of length 3 after receiving an argument of array length 4.
-      holder.push(holder);
     } else { // when rolling at (dis)advantage, simply roll twice and return the results
       holder.push(cb(cbArg));
     };
   };
+  console.log(holder)
   return holder;
 };
