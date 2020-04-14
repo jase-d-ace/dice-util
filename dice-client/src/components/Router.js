c1415b19e8acafc4d747610dba4afa7dc2dcb057
Jason Andrada <jpa323@nyu.edu>
2020-04-14 19:44:31 -0400

    render nav component

--- dice-client/src/components/Router.js
+++ dice-client/src/components/Router.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from '../App';
 import Search from './Search';
+import Nav from './Nav';
 
 function Router() {
   return (
     <BrowserRouter>
+      <Nav />
       <Switch>
         <Route exact path="/" component={App} />
         <Route exact path="/search" component={Search} />
