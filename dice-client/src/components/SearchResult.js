c12592680fed3a69ef3a51ec57e587fa4e171b57
Jason Andrada <jpa323@nyu.edu>
2020-04-14 19:03:00 -0400

    add section for legendary actions

--- dice-client/src/components/SearchResult.js
+++ dice-client/src/components/SearchResult.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function SearchResult({result: { name, actions, armor_class, charisma, strength, dexterity, wisdom, intelligence, constitution, damage_immunities, damage_resistances, hit_points, damage_vulnerabilities }}) {
+function SearchResult({result: { name, actions, armor_class, charisma, strength, dexterity, wisdom, intelligence, constitution, damage_immunities, damage_resistances, hit_points, damage_vulnerabilities, legendary_actions, legendary_desc }}) {
   let id = 0;
   return (
     <div className="result">
@@ -25,6 +25,19 @@ function SearchResult({result: { name, actions, armor_class, charisma, strength,
         <p>Resistances: {damage_resistances ? damage_resistances : 'none'}</p>
         <p>Vulnerabilities: {damage_vulnerabilities ? damage_vulnerabilities : 'none'}</p>
       </div>
+    {legendary_actions ? (
+      <div className="legendary-actions">
+        <h3>Legendary Actions</h3>
+        <p>{legendary_desc}</p>
+        <ul>
+          {
+            legendary_actions.map(({name: legAct, desc}) =>(
+              <li key={id++}>{legAct}: {desc}</li>
+            ))
+          }
+        </ul>
+      </div>
+    ) : ""}
     </div>
   );
 };
