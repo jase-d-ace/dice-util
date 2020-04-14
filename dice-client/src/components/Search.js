f8d93793e2d572e8ae7be0fbe8a89f90d0c7c605
Jason Andrada <jpa323@nyu.edu>
2020-04-14 14:46:20 -0400

    add form and callbacks to search dnd 5e api

--- dice-client/src/components/Search.js
+++ dice-client/src/components/Search.js
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import _ from 'underscore';
+import { handleInputChange, handleFormSubmit } from '../services';
+import lionhead from '../images/lionhead.png'
+
 
 function Search() {
   //state values
   const [searchTerm, setSearchTerm] = useState(null);
+  const [prevSearchTerm, setPrevSearchTerm] = useState(null);
   const [searchResult, setSearchResult] = useState(null);
+
+  useEffect(() => {
+    if(searchResult) {
+      setPrevSearchTerm(searchResult.name)
+    }
+  }, [searchResult])
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Search for some monsters</h1>
-        <form>
-          <input type="text" required placeholder="Search for monsters" />
+        <img src={lionhead} className="App-logo" alt="logo" />
+        <h1>{searchResult ? `Your last search: ${prevSearchTerm}` : "Search for monsters" }</h1>
+        <form onSubmit={(e) => handleFormSubmit(e, searchTerm, setSearchResult)}>
+          <input type="text" onChange={(e) => handleInputChange(setSearchTerm, e.target.value)} required placeholder="Search for monsters" />
           <input type="submit" value="search!" />
         </form>
       </header>
