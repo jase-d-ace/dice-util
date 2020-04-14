36ab05a9134253de8ce75741668f132adde7a965
Jason Andrada <jpa323@nyu.edu>
2020-04-14 10:59:37 -0400

    create router file

---
+++ dice-client/src/components/Router.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import App from '../App';
+
+function Router() {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
