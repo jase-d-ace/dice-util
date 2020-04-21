e3d7f5682c87918b7a45fe2a46e8c8bba722e7d5
Jason Andrada <jpa323@nyu.edu>
2020-04-21 12:25:37 -0400

    create form and start state logic

--- dice-client/src/components/Initiatives.js
+++ dice-client/src/components/Initiatives.js
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import helmet from '../images/helmet.png'
 
-const Initiatives = () => <h1>Hello world</h1>;
+
+// useState with prevState callback: 
+// https://stackoverflow.com/questions/54150783/react-hooks-usestate-with-object
+function Initiatives() {
+  const [character, setCharacter] = useState({
+    charName: null,
+    initiative: null
+  });
+  const [order, setOrder] = useState([]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={helmet} className="App-logo" alt="logo" />
+        <h1>Let's Play</h1>
+        <div className="form-container">
+          <form className="monster-search">
+            <input type="text" required placeholder="name of character" />
+            <input type="submit" value="search!" />
+          </form>
+        </div>
+      </header>
+    </div>
+  );
+};
 
 export default Initiatives;
