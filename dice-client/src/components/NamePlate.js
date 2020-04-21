680bd62d602a9260013efe5749f215c8e3dc1872
Jason Andrada <jpa323@nyu.edu>
2020-04-21 17:06:29 -0400

    add classes for styling

--- dice-client/src/components/NamePlate.js
+++ dice-client/src/components/NamePlate.js
@@ -3,9 +3,8 @@ import { handleInitiativeOrder } from '../services';
 
 function NamePlate({char: { charName, initiative }, setOrder}) {
   return (
-    <li>
-      Name: {charName}, Initiative: {initiative}
-      <button onClick={() => handleInitiativeOrder(null, setOrder, {character: {charName, initiative}, type: "remove"})}>Remove</button>
+    <li className="nameplate">
+      Name: {charName} | Initiative: {initiative} | <button className="button" onClick={() => handleInitiativeOrder(null, setOrder, {character: {charName, initiative}, type: "remove"})}>Remove</button>
     </li>
   );
 };
