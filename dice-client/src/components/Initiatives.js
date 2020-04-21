7c41099d7cdf79cfc89bee9c754225098cc8efe4
Jason Andrada <jpa323@nyu.edu>
2020-04-21 12:56:39 -0400

    add state management function to form

--- dice-client/src/components/Initiatives.js
+++ dice-client/src/components/Initiatives.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import helmet from '../images/helmet.png'
+import { handleFormChange } from '../services';
 
 
 // useState with prevState callback: 
@@ -18,7 +19,8 @@ function Initiatives() {
         <h1>Let's Play</h1>
         <div className="form-container">
           <form className="monster-search">
-            <input type="text" required placeholder="name of character" />
+            <input type="text" name="charName" required placeholder="name of character" onChange={(e) => handleFormChange(setCharacter, e, character)} />
+            <input type="number" name="initiative" onChange={(e) => handleFormChange(setCharacter, e, character)} required min="0" max="30" placeholder="initiative" step="1" />
             <input type="submit" value="search!" />
           </form>
         </div>
