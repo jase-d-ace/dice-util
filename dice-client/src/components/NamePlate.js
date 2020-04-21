6ec34fabd7d3bdd3c56466952c49cf10efac28bf
Jason Andrada <jpa323@nyu.edu>
2020-04-21 16:46:26 -0400

    add remove button to name plate

--- dice-client/src/components/NamePlate.js
+++ dice-client/src/components/NamePlate.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { handleInitiativeOrder } from '../services';
 
-function NamePlate({char: { charName, initiative }}) {
+function NamePlate({char: { charName, initiative }, setOrder}) {
   return (
     <li>
       Name: {charName}, Initiative: {initiative}
+      <button onClick={() => handleInitiativeOrder(null, setOrder, {character: {charName, initiative}, type: "remove"})}>Remove</button>
     </li>
   );
 };
