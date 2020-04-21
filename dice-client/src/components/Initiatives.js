2177a2c46840c756993032a799c1d77e54db42a9
Jason Andrada <jpa323@nyu.edu>
2020-04-21 14:38:43 -0400

    import useReducer hook and implement

--- dice-client/src/components/Initiatives.js
+++ dice-client/src/components/Initiatives.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import helmet from '../images/helmet.png'
-import { handleFormChange } from '../services';
+import { handleFormChange, reducer, handleInitiativeOrder } from '../services';
 
 
 // useState with prevState callback: 
@@ -10,7 +10,7 @@ function Initiatives() {
     charName: null,
     initiative: null
   });
-  const [order, setOrder] = useState([]);
+  const [order, setOrder] = useReducer(reducer, []);
 
   return (
     <div className="App">
@@ -18,7 +18,7 @@ function Initiatives() {
         <img src={helmet} className="App-logo" alt="logo" />
         <h1>Let's Play</h1>
         <div className="form-container">
-          <form className="monster-search">
+          <form className="monster-search" onSubmit={(e)=>handleInitiativeOrder(e, setOrder, {character, type: "add"})}>
             <input type="text" name="charName" required placeholder="name of character" onChange={(e) => handleFormChange(setCharacter, e, character)} />
             <input type="number" name="initiative" onChange={(e) => handleFormChange(setCharacter, e, character)} required min="0" max="30" placeholder="initiative" step="1" />
             <input type="submit" value="search!" />
