070172fd280b33bd7e9a3124171c9c1d852c63f0
Jason Andrada <jpa323@nyu.edu>
2020-04-21 11:56:23 -0400

    open route to initiatives functionality

--- dice-client/src/components/Router.js
+++ dice-client/src/components/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from '../App';
 import Search from './Search';
+import Initiatives from './Initiatives';
 import Nav from './Nav';
 
 function Router() {
@@ -11,6 +12,7 @@ function Router() {
       <Switch>
         <Route exact path="/" component={App} />
         <Route exact path="/search" component={Search} />
+        <Route exact path="/initiatives" component={Initiatives} />
       </Switch>
     </BrowserRouter>
   );
