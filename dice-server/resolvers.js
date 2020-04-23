4074287576e32b9b8a42054b601f3d0daaf5efde
Jason Andrada <jpa323@nyu.edu>
2020-04-23 12:29:15 -0400

    add query methods

--- dice-server/resolvers.js
+++ dice-server/resolvers.js
@@ -4,6 +4,12 @@ const resolvers = {
   Query: {
     notes() {
       return db.any(`SELECT * FROM notes`)
+    },
+    note(_, { author }) {
+      return db.one(`SELECT * FROM notes WHERE author = $1`, author)
+    },
+    notesByAuthor(_, { author }) {
+      return db.any(`SELECT * FROM notes WHERE author = $1`, author)
     }
   }
 }
