73f38053d4a423b1dd377f33febfaaf9aaf70c6b
Jason Andrada <jpa323@nyu.edu>
2020-04-23 18:30:09 -0400

    configure apollo client and wrap app

--- dice-client/src/index.js
+++ dice-client/src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 // import App from './App';
 import Router from './components/Router';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000'
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router />
+    <ApolloProvider client={client}>
+      <Router />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
