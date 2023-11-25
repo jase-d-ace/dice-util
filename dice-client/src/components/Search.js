dd2a2e0adbcfe0d1cb50a1dedf5f21accb55fcbd
Jason Andrada <andrada.jasonp@gmail.com>
2023-11-25 01:43:30 -0500

    styles for top-level search component

--- dice-client/src/components/Search.js
+++ dice-client/src/components/Search.js
@@ -12,16 +12,20 @@ function Search() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={lionhead} className="App-logo" alt="logo" />
-                <h1>{searchResult ? `Your last search: ${searchResult.name || searchResult.term}` : "Search for monsters"}</h1>
-                <div className="form-container">
-                    <form className="monster-search" onSubmit={(e) => handleFormSubmit(e, searchTerm, setSearchResult)}>
-                        <input type="text" onChange={(e) => handleInputChange(setSearchTerm, e.target.value)} required placeholder="Search for monsters" />
-                        <input type="submit" value="search!" />
-                    </form>
-                </div>
-                <div className="results-container">
-                    {searchResult ? searchResult.name ? <SearchResult result={searchResult} /> : (<h1>your search returned no results</h1>) : ""}
+                <div className="search-page">
+                    <div className="search-form">
+                        <img src={lionhead} className="App-logo" alt="logo" />
+                        <div className="header">{searchResult ? `Your last search: ${searchResult.name || searchResult.term}` : "Search for monsters"}</div>
+                        <div className="form-container">
+                            <form className="monster-search" onSubmit={(e) => handleFormSubmit(e, searchTerm, setSearchResult)}>
+                                <input type="text" onChange={(e) => handleInputChange(setSearchTerm, e.target.value)} required placeholder="Search for monsters" />
+                                <input className="submit" type="submit" value="search!" />
+                            </form>
+                        </div>
+                    </div>
+                    <div className="results-container" style={{display: searchResult ? "flex" : "none"}}>
+                        {searchResult ? searchResult.name ? <SearchResult result={searchResult} /> : (<div className="header">your search returned no results</div>) : ""}
+                    </div>
                 </div>
             </header>
         </div>
