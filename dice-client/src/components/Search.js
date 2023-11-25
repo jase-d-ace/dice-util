c75e65e6a41a78f482e7d20b8967a73741195b55
Jason Andrada <andrada.jasonp@gmail.com>
2023-11-25 00:20:55 -0500

    refactor search component to remove a lot of unneeded code

--- dice-client/src/components/Search.js
+++ dice-client/src/components/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { handleInputChange, handleFormSubmit } from '../services';
 import lionhead from '../images/lionhead.png'
 import SearchResult from './SearchResult';
@@ -7,33 +7,13 @@ import SearchResult from './SearchResult';
 function Search() {
     //state values
     const [searchTerm, setSearchTerm] = useState(null);
-    const [prevSearchTerm, setPrevSearchTerm] = useState(null);
-    const [error, setError] = useState(false);
-    const [searchCompleted, setSearchCompleted] = useState(false);
     const [searchResult, setSearchResult] = useState(null);
-
-    useEffect(() => {
-        if (searchResult) {
-            setSearchCompleted(true);
-            if (searchResult.name) {
-                setPrevSearchTerm(searchResult.name);
-                setError(false);
-            } else {
-                setError(true);
-            }
-        }
-
-        return () => {
-            setPrevSearchTerm(searchTerm);
-            setSearchCompleted(false);
-        }
-    }, [searchCompleted, searchResult, error])
 
     return (
         <div className="App">
             <header className="App-header">
                 <img src={lionhead} className="App-logo" alt="logo" />
-                <h1>{searchResult ? `Your last search: ${prevSearchTerm}` : "Search for monsters"}</h1>
+                <h1>{searchResult ? `Your last search: ${searchResult.name || searchResult.term}` : "Search for monsters"}</h1>
                 <div className="form-container">
                     <form className="monster-search" onSubmit={(e) => handleFormSubmit(e, searchTerm, setSearchResult)}>
                         <input type="text" onChange={(e) => handleInputChange(setSearchTerm, e.target.value)} required placeholder="Search for monsters" />
@@ -41,7 +21,7 @@ function Search() {
                     </form>
                 </div>
                 <div className="results-container">
-                    {error ? (<h1>your search returned no results</h1>) : (searchResult && searchResult.name) ? <SearchResult result={searchResult} /> : ""}
+                    {searchResult ? searchResult.name ? <SearchResult result={searchResult} /> : (<h1>your search returned no results</h1>) : ""}
                 </div>
             </header>
         </div>
