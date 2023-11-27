6319304dd00686c277c22e644062b3fde108f181
Jason Andrada <andrada.jasonp@gmail.com>
2023-11-27 01:03:39 -0500

    start adding hamburger stuff

--- dice-client/src/components/Nav.js
+++ dice-client/src/components/Nav.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Hamburger from "./Hamburger";
 
-function Nav() {
+export default function Nav() {
+    const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+
+    const toggleHamburger = () => setIsHamburgerOpen(!isHamburgerOpen)
     return (
         <nav className="nav">
-            <li className="link"><Link to="/">Dice</Link></li>
-            <li className="link"><Link to="/search">Monsters</Link></li>
-            <li className="link"><Link to="/initiatives">Initiatives</Link></li>
+            <div className="mobile-nav" onClick={toggleHamburger}>
+                <Hamburger />
+            </div>
+            <ul className={`navigation ${isHamburgerOpen ? "mobile-active" : ""}`}>
+                <span className="nav-site-header">Roll Some Dice</span>
+                <li className="link"><Link to="/">Dice</Link></li>
+                <li className="link"><Link to="/search">Monsters</Link></li>
+                <li className="link"><Link to="/initiatives">Initiatives</Link></li>
+            </ul>
         </nav>
     );
 };
-
-export default Nav;
