3feb69b1348b23e657a7f3182d1b46e0d82720d1
Jason Andrada <andrada.jasonp@gmail.com>
2023-11-27 01:03:47 -0500

    create hamburger

---
+++ dice-client/src/components/Hamburger.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hamburger() {
+    return (
+        <div class="hamburger-nav">
+            <div className="burger-part burger-one"></div>
+            <div className="burger-part burger-two"></div>
+            <div className="burger-part burger-three"></div>
+        </div>
+    )
+}
